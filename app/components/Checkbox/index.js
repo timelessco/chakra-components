415faac235284f9fe06c76dd24e033ebbf16ae76
Navin <navin007.a@gmail.com>
2020-03-26 21:27:10 +0530

    feat(checkbox index.js): add Control Box for Checkbox of Chakra to create custom checkbox
    [RFC] Checkbox Component #16

--- app/components/Checkbox/index.js
+++ app/components/Checkbox/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
-import { Checkbox as ChakraCheckbox } from '@chakra-ui/core';
+import { VisuallyHidden, ControlBox, Icon, Box } from '@chakra-ui/core';
 import PropTypes from 'prop-types';
 
 const Checkbox = ({ children }) => (
-  <ChakraCheckbox variantColor="brand">{children}</ChakraCheckbox>
+  <Box lineHeight="14px">
+    <label style={{ lineHeight: 'inherit' }}>
+      {/* This is the sibling input, it's visually hidden */}
+      <VisuallyHidden as="input" type="checkbox" defaultChecked />
+
+      {/* This is the control box with a check icon as children */}
+      <ControlBox
+        borderWidth="1.5px"
+        size="14px"
+        rounded="sm"
+        _checked={{ bg: 'brand.500', color: 'white', borderColor: 'brand.500' }}
+      >
+        <Icon name="check" size="10px" />
+      </ControlBox>
+
+      {/* You can pass additional text */}
+      <Box
+        as="span"
+        verticalAlign="top"
+        ml={3}
+        fontFamily="body"
+        fontSize="13px"
+        fontWeight="medium"
+        color="#333536"
+        letterSpacing="0.13px"
+      >
+        {children}
+      </Box>
+    </label>
+  </Box>
 );
 
 Checkbox.propTypes = {
