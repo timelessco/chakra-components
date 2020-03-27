e63b6a4775e7cdd6e63ebcdffa36453a1f5ca8dd
Navin <navin007.a@gmail.com>
2020-03-27 20:30:38 +0530

    fix(checkbox): fix the isDisabled cursor blackhole bug

--- app/components/Checkbox/index.js
+++ app/components/Checkbox/index.js
@@ -15,64 +15,64 @@ const Checkbox = ({
   name,
   value,
 }) => (
-  <Box lineHeight="14px">
-    <label style={{ lineHeight: 'inherit' }}>
-      {/* This is the sibling input, it's visually hidden */}
-      <VisuallyHidden
-        as="input"
-        type="checkbox"
-        id={id}
-        name={name}
-        value={value}
-        disabled={isDisabled}
-        checked={isChecked}
-        onBlur={onBlur}
-        onFocus={onFocus}
-        onChange={onChange}
-        defaultChecked={defaultIsChecked}
-      />
+  <Box
+    as="label"
+    lineHeight="14px"
+    cursor={isDisabled ? 'not-allowed' : 'pointer'}
+  >
+    {/* This is the sibling input, it's visually hidden */}
+    <VisuallyHidden
+      as="input"
+      type="checkbox"
+      id={id}
+      name={name}
+      value={value}
+      disabled={isDisabled}
+      checked={isChecked}
+      onBlur={onBlur}
+      onFocus={onFocus}
+      onChange={onChange}
+      defaultChecked={defaultIsChecked}
+    />
 
-      {/* This is the control box with a check icon as children */}
-      <ControlBox
-        cursor={isDisabled ? 'not-allowed' : 'pointer'}
-        borderWidth="1.5px"
-        size="14px"
-        borderColor={
-          isDisabled ? 'checkbox.disabled.borderColor' : 'checkbox.borderColor'
-        }
-        rounded="md"
-        _checked={
-          isDisabled
-            ? {}
-            : { bg: 'brand.500', color: 'white', borderColor: 'brand.500' }
-        }
-        _focus={{ borderColor: 'outline' }}
-        _hover={
-          isDisabled
-            ? {}
-            : {
-                borderColor: 'brand.500',
-                shadow: '0 0 0 2px #31a7d840, inset 0 0 0 2px #31a7d840',
-              }
-        }
-      >
-        <Icon name="customCheck" size="10px" />
-      </ControlBox>
+    {/* This is the control box with a check icon as children */}
+    <ControlBox
+      borderWidth="1.5px"
+      size="14px"
+      borderColor={
+        isDisabled ? 'checkbox.disabled.borderColor' : 'checkbox.borderColor'
+      }
+      rounded="md"
+      _checked={
+        isDisabled
+          ? {}
+          : { bg: 'brand.500', color: 'white', borderColor: 'brand.500' }
+      }
+      _focus={{ borderColor: 'outline' }}
+      _hover={
+        isDisabled
+          ? {}
+          : {
+              borderColor: 'brand.500',
+              shadow: '0 0 0 2px #31a7d840, inset 0 0 0 2px #31a7d840',
+            }
+      }
+    >
+      <Icon name="customCheck" size="10px" />
+    </ControlBox>
 
-      {/* You can pass additional text */}
-      <Box
-        cursor={isDisabled ? 'not-allowed' : 'pointer'}
-        as="span"
-        ml={2}
-        fontFamily="body"
-        fontSize="13px"
-        fontWeight="medium"
-        color={isDisabled ? 'checkbox.disabled.color' : 'checkbox.color'}
-        letterSpacing="0.13px"
-      >
-        {children}
-      </Box>
-    </label>
+    {/* You can pass additional text */}
+    <Box
+      as="span"
+      ml={2}
+      fontFamily="body"
+      fontSize="13px"
+      fontWeight="medium"
+      color={isDisabled ? 'checkbox.disabled.color' : 'checkbox.color'}
+      letterSpacing="0.13px"
+    >
+      {children}
+    </Box>
   </Box>
 );
 
