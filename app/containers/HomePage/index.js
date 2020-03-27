4d5f42a5526e5f8036cb38d1db5fd9a475834a80
Navin <navin007.a@gmail.com>
2020-03-27 17:38:51 +0530

    chore(example): add more examples for each prop exposed

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { Box } from '@chakra-ui/core';
+import { Box, Text } from '@chakra-ui/core';
 import Checkbox from '../../components/Checkbox';
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
@@ -15,13 +15,40 @@ export default function HomePage() {
   return (
     <>
       <Box p="4">
+        <Box fontSize="xl" mb="1">
+          Normal
+        </Box>
         <Checkbox>One</Checkbox>
       </Box>
       <Box p="4">
-        <Checkbox>Two</Checkbox>
+        <Box fontSize="xl" mb="1">
+          Checked
+        </Box>
+        <Checkbox isChecked>Two</Checkbox>
       </Box>
       <Box p="4">
-        <Checkbox isDisabled>Three</Checkbox>
+        <Box fontSize="xl" mb="1">
+          Disabled
+        </Box>
+        <Checkbox isDisabled>Two</Checkbox>
+      </Box>
+      <Box p="4">
+        <Box fontSize="xl" mb="1">
+          onBlur
+        </Box>
+        <Checkbox onBlur={() => console.log('Blur')}>Three</Checkbox>
+      </Box>
+      <Box p="4">
+        <Box fontSize="xl" mb="1">
+          onFocus
+        </Box>
+        <Checkbox onFocus={() => console.log('Focus')}>Three</Checkbox>
+      </Box>
+      <Box p="4">
+        <Box fontSize="xl" mb="1">
+          onChange
+        </Box>
+        <Checkbox onChange={() => console.log('Change')}>Three</Checkbox>
       </Box>
     </>
   );
