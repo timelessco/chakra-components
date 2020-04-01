7b5ccb99d2537118b7b947702461d4fde2cf0ba0
Navin <navin007.a@gmail.com>
2020-04-01 16:25:05 +0530

    docs(text): add docz for Text Component

--- docs/src/components/withTheme.js
+++ docs/src/components/withTheme.js
@@ -3,12 +3,10 @@ import { Box, ThemeProvider } from '@chakra-ui/core';
 
 import theme from '../../../src/theme';
 
-export function withTheme(OriginalComponent) {
-  return props => (
-    <ThemeProvider theme={theme}>
-      <Box p={2}>
-        <OriginalComponent {...props} />
-      </Box>
-    </ThemeProvider>
-  );
-}
+export const withTheme = Component => props => (
+  <ThemeProvider theme={theme}>
+    <Box p={2}>
+      <Component {...props} />
+    </Box>
+  </ThemeProvider>
+);
