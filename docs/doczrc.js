79aacec2be745d4bf7cd45a270bba4b63dd52d89
Prasanna <prasanna.krystal@gmail.com>
2020-04-01 23:51:32 +0530

    removed docs folder

--- docs/doczrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  title: 'Chakra UI Components',
-  description: 'A set of reusable wrapper components for chakra UI.',
-  menu: [
-    'Getting Started',
-    { name: 'Components', menu: ['Text', 'Heading', 'Checkbox'] },
-  ],
-  ignore: ['README.md'],
-};
--- docs/gatsby-node.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-exports.onCreateWebpackConfig = arg => {
-  arg.actions.setWebpackConfig({
-    resolve: {
-      modules: [path.resolve(__dirname), './docs/node_modules'],
-    },
-  });
-};
