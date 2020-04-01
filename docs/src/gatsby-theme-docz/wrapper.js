584cffba6c9e53887bbd8baa44e812f31a87cb00
Navin <navin007.a@gmail.com>
2020-04-01 14:11:14 +0530

    docs(layout): add CSSReset for the whole code layout as wrapper with shadow components

---
+++ docs/src/gatsby-theme-docz/wrapper.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+
+import theme from '../../../src/theme';
+
+const Wrapper = ({ children }) => (
+  <React.Fragment>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+    </ThemeProvider>
+    {children}
+  </React.Fragment>
+);
+
+export default Wrapper;
