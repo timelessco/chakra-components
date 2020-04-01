0619d432c8ff4da7b80ca560d1f8d2e413d582fc
Navin <navin007.a@gmail.com>
2020-04-01 16:14:10 +0530

    feat(hoc for docz): create a wrapper for theme provider - playground

--- docs/src/pages/heading/index.js
+++ docs/src/pages/heading/index.js
@@ -1,35 +1,11 @@
-import React from 'react';
-import { Box, ThemeProvider } from '@chakra-ui/core';
+import { Heading as BaseHeading } from '../../../../src/components/Heading';
+import { withTheme } from '../../components/withTheme';
 
-import { Heading as HeadingWrapper } from '../../../../src/components/Heading';
-
-import theme from '../../../../src/theme';
-
-const Heading = props => (
-  <ThemeProvider theme={theme}>
-    <Box p={2}>
-      <HeadingWrapper {...props} />
-    </Box>
-  </ThemeProvider>
-);
-
-const H1 = props => (
-  <ThemeProvider theme={theme}>
-    <Box p={2}>
-      <HeadingWrapper.H1 {...props} />
-    </Box>
-  </ThemeProvider>
-);
-
-const H2 = props => (
-  <ThemeProvider theme={theme}>
-    <Box p={2}>
-      <HeadingWrapper.H2 {...props} />
-    </Box>
-  </ThemeProvider>
-);
+const Heading = withTheme(BaseHeading);
+const H1 = withTheme(BaseHeading.H1);
+const H2 = withTheme(BaseHeading.H2);
 
 Heading.H1 = H1;
 Heading.H2 = H2;
 
-export { Heading };
+export default Heading;
