f109c207593a1fe406677a6dfaa48243010e728b
Navin <navin007.a@gmail.com>
2020-04-01 14:13:59 +0530

    docs(heading): add docz for Heading components

--- docs/src/pages/heading/index.js
+++ docs/src/pages/heading/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 
-import { Box, ThemeProvider, CSSReset } from '@chakra-ui/core';
+import { Box, ThemeProvider } from '@chakra-ui/core';
 
 import theme from '../../../../src/theme';
 import { Heading as HeadingWrapper } from '../../../../src/components/Heading';
 
-export const Heading = props => (
+const Heading = props => (
   <ThemeProvider theme={theme}>
-    <CSSReset />
     <Box p={2}>
       <HeadingWrapper {...props} />
     </Box>
   </ThemeProvider>
 );
+
+const H1 = props => (
+  <ThemeProvider theme={theme}>
+    <Box p={2}>
+      <HeadingWrapper.H1 {...props} />
+    </Box>
+  </ThemeProvider>
+);
+
+const H2 = props => (
+  <ThemeProvider theme={theme}>
+    <Box p={2}>
+      <HeadingWrapper.H2 {...props} />
+    </Box>
+  </ThemeProvider>
+);
+
+Heading.H1 = H1;
+Heading.H2 = H2;
+
+export { Heading };
