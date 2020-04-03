4a42d0d72dc9360781cacdef20991c79323e89d0
Navin <navin007.a@gmail.com>
2020-04-03 21:16:57 +0530

    feat(button): dot notation variants for button

--- packages/components/button/index.js
+++ packages/components/button/index.js
@@ -1,38 +1,85 @@
 /* eslint-disable indent */
 import React from 'react';
-import { Button as ChakraButton, ThemeProvider } from '@chakra-ui/core';
+import {
+  Button as ChakraButton,
+  ThemeProvider,
+  Link as ChakraLink,
+} from '@chakra-ui/core';
 import PropTypes from 'prop-types';
-import theme from '../../helpers/theme';
 
-// eslint-disable-next-line react/prop-types
+import theme from '../../helpers/theme';
+import {
+  primary,
+  solid,
+  ghost,
+  link,
+  outline,
+  secondary,
+  rightIcon,
+  leftIcon,
+} from './styles';
+
 const Button = ({ children, ...props }) => (
   <ThemeProvider theme={theme}>
-    <ChakraButton {...props}>
-      {typeof children === 'function' ? children() : children}
-    </ChakraButton>
+    <ChakraButton {...props}>{children}</ChakraButton>
   </ThemeProvider>
 );
 
-const link = ({ href, target, ...props }) => (
-  <Button as="a" href={href} target={target} {...props} />
+const Solid = ({ ...props }) => (
+  <Button variant="solid" {...solid} {...props} />
 );
 
-Button.Link = link;
+const Outline = ({ ...props }) => (
+  <Button variant="outline" {...outline} {...props} />
+);
+
+const Ghost = ({ ...props }) => (
+  <Button variant="ghost" {...ghost} {...props} />
+);
+
+const Link = ({ href, isExternal, children, ...props }) => (
+  <Button variant="link" {...link} {...props}>
+    <ChakraLink href={href} isExternal={isExternal}>
+      {children}
+    </ChakraLink>
+  </Button>
+);
+
+const LeftIcon = ({ ...props }) => <Button {...leftIcon} {...props} />;
+
+const RightIcon = ({ ...props }) => <Button {...rightIcon} {...props} />;
+
+const Primary = ({ ...props }) => (
+  <Button variant="solid" {...primary} {...props} />
+);
+
+const Secondary = ({ ...props }) => (
+  <Button variant="outline" {...secondary} {...props} />
+);
 
 Button.propTypes = {
+  children: PropTypes.any,
   size: PropTypes.string,
   isDisabled: PropTypes.bool,
   varient: PropTypes.string,
   variantColor: PropTypes.string,
   isLoading: PropTypes.bool,
   loadingText: PropTypes.string,
-  href: PropTypes.string,
-  target: PropTypes.string,
 };
 
-Button.defaultProps = {
-  href: 'https://chakra-ui.com/',
-  target: 'blank',
+Link.propTypes = {
+  children: PropTypes.any,
+  href: PropTypes.string,
+  isExternal: PropTypes.bool,
 };
+
+Button.Link = Link;
+Button.Primary = Primary;
+Button.Secondary = Secondary;
+Button.Solid = Solid;
+Button.Outline = Outline;
+Button.Ghost = Ghost;
+Button.LeftIcon = LeftIcon;
+Button.RightIcon = RightIcon;
 
 export default Button;
---
+++ packages/components/button/styles.js
@@ -0,0 +1,41 @@
+export const solid = {
+  variantColor: 'blue',
+  size: 'xs',
+};
+
+export const outline = {
+  variantColor: 'pink',
+  size: 'sm',
+};
+
+export const ghost = {
+  variantColor: 'purple',
+  size: 'md',
+};
+
+export const link = {
+  variantColor: 'teal',
+  size: 'lg',
+};
+
+export const rightIcon = {
+  leftIcon: 'email',
+  varaint: 'solid',
+  variantColor: 'orange',
+};
+
+export const leftIcon = {
+  rightIcon: 'arrow-forward',
+  variant: 'outline',
+  variantColor: 'yellow',
+};
+
+export const primary = {
+  variantColor: 'teal',
+  size: 'md',
+};
+
+export const secondary = {
+  variantColor: 'green',
+  size: 'md',
+};
