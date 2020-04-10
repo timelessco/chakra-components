45cad68200dd747ac3efcd6daab4a6e2b9438bf3
Navin <navin007.a@gmail.com>
2020-04-10 08:24:17 +0530

    refactor(menu): clone Chakra menu button for learning

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,52 +1,7 @@
 /* eslint-disable indent */
 import React from 'react';
-import { ThemeProvider, Box, Flex, PseudoBox } from '@chakra-ui/core';
-import theme from '../../theme';
-console.log('%ctheme', 'color: #f2ceb6', theme);
+import { Box } from '@chakra-ui/core';
 
-const MenuItem = ({ children, isActive, isDisabled, ...props }) => (
-  <PseudoBox
-    p="5"
-    color="gray.400"
-    cursor="pointer"
-    {...isActive && {
-      color: 'blue.500',
-      shadow: 'menu',
-    }}
-    {...(isDisabled
-      ? {
-          cursor: 'not-allowed',
-          opacity: '40%',
-        }
-      : {
-          _hover: {
-            color: 'blue.500',
-            shadow: 'menu',
-          },
-        })}
-    {...props}
-  >
-    {children}
-  </PseudoBox>
-);
+const Menu = () => <Box>Menu</Box>;
 
-MenuItem.displayName = MenuItem;
-
-const Menu = ({ children, ...props }) => (
-  <ThemeProvider theme={theme}>
-    <Box
-      fontFamily="body"
-      fontWeight="medium"
-      fontSize="md"
-      lineHeight="shorter"
-    >
-      <Flex align="center" {...props}>
-        {children}
-      </Flex>
-    </Box>
-  </ThemeProvider>
-);
-
-Menu.displayName = Menu;
-
-export { Menu, MenuItem };
+export { Menu };
---
+++ src/components/Menu/our-test-menu.js
@@ -0,0 +1,48 @@
+/* eslint-disable indent */
+import React from 'react';
+import { ThemeProvider, Box, Flex, PseudoBox } from '@chakra-ui/core';
+import theme from '../../theme';
+console.log('%ctheme', 'color: #f2ceb6', theme);
+
+const MenuItem = ({ children, isActive, isDisabled, ...props }) => (
+  <PseudoBox
+    p="5"
+    color="gray.400"
+    cursor="pointer"
+    {...(isActive && {
+      color: 'blue.500',
+      shadow: 'menu',
+    })}
+    {...(isDisabled
+      ? {
+          cursor: 'not-allowed',
+          opacity: '40%',
+        }
+      : {
+          _hover: {
+            color: 'blue.500',
+            shadow: 'menu',
+          },
+        })}
+    {...props}
+  >
+    {children}
+  </PseudoBox>
+);
+
+const Menu = ({ children, ...props }) => (
+  <ThemeProvider theme={theme}>
+    <Box
+      fontFamily="body"
+      fontWeight="medium"
+      fontSize="md"
+      lineHeight="shorter"
+    >
+      <Flex align="center" {...props}>
+        {children}
+      </Flex>
+    </Box>
+  </ThemeProvider>
+);
+
+export { Menu, MenuItem };
