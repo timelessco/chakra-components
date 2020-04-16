b71dc3b383616c75c2ef9d1ee9fdf3c8de98c1f3
Navin <navin007.a@gmail.com>
2020-04-16 13:12:53 +0530

    fix(ci): fix refresh page error

--- docs/next.config.js
+++ docs/next.config.js
@@ -12,6 +12,7 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "md", "mdx"],
+  exportTrailingSlash: true,
   exportPathMap: function () {
     return {
       "/": { page: "/" },
