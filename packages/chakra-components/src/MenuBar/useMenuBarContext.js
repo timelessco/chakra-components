43530139510b60ab022ebb6d6c52527387ab9344
Navin Moorthy <navin007.a@gmail.com>
2020-04-17 12:22:48 +0530

    Update useMenuBarContext.js

--- packages/chakra-components/src/MenuBar/useMenuBarContext.js
+++ packages/chakra-components/src/MenuBar/useMenuBarContext.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { MenuBarContext } from "./menuBar";
+import { MenuBarContext } from "./menubar";
 
 export const useMenuBarContext = () => {
   const context = useContext(MenuBarContext);
