4445329a06bb33262a653842d558fd301231cf91
Navin <navin007.a@gmail.com>
2020-04-17 12:00:15 +0530

    chore(ci): fix build error in ci

--- packages/chakra-components/src/MenuBar/index.js
+++ packages/chakra-components/src/MenuBar/index.js
@@ -1,6 +1,6 @@
+export * from "./MenuBar";
 export * from "./MenuBarItem";
 export * from "./SubMenu";
 export * from "./SubMenuTitle";
 export * from "./SubMenuList";
 export * from "./SubMenuItem";
-export * from "./MenuBar";
