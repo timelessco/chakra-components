f0e8f7ae95526ce0deb572c27de27f1cf736d08a
Navin <navin007.a@gmail.com>
2020-04-28 14:47:50 +0530

    feat(combobox): add combobox starter code

--- packages/chakra-components/src/ComboBox/index.js
+++ packages/chakra-components/src/ComboBox/index.js
@@ -1,130 +1,104 @@
-import React, { useRef } from "react";
-import useSelect from "./useSelect";
-import styled from "@emotion/styled";
-import matchSorter from "match-sorter";
-import { FixedSizeList } from "react-window";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import { forwardRef, cloneElement } from "react";
+import {
+  Box,
+  Input,
+  Icon,
+  useTheme,
+  InputRightElement,
+  InputLeftElement,
+  InputLeftAddon,
+  InputRightAddon,
+} from "@chakra-ui/core";
+import { cleanChildren } from "@chakra-ui/core/dist/utils";
 
-const OptionsWrapper = styled.div`
-  position: absolute;
-  top: 100%;
-  left: 0;
-  z-index: 1;
-`;
+import { inputSizes } from "@chakra-ui/core/dist/Input/styles";
 
-const Options = styled(FixedSizeList)`
-  border: 1px solid black;
-`;
-
-const Option = styled.div`
-  background: ${props =>
-    props.highlighted ? "lightblue" : props.selected ? "lightgray" : "white"};
-  display: flex;
-  alignitems: center;
-  padding: 0.5rem;
-`;
-
-const MySelect = ({
-  value,
-  options,
-  onChange,
-  multi,
-  pageSize = 10,
-  itemHeight = 40,
-}) => {
-  const reactWindowInstanceRef = useRef();
-  const optionsRef = useRef();
-
-  const scrollToIndex = index => {
-    if (!reactWindowInstanceRef.current) {
-      return;
-    }
-    reactWindowInstanceRef.current.scrollToItem(index);
-  };
-
-  const shiftAmount = pageSize;
-
-  const {
-    visibleOptions,
-    selectedOption,
-    highlightedOption,
-    getInputProps,
-    getOptionProps,
-    isOpen,
-  } = useSelect({
-    multi,
-    options,
-    value,
-    onChange,
-    scrollToIndex,
-    optionsRef,
-    shiftAmount,
-    filterFn: (options, value) =>
-      matchSorter(options, value, { keys: ["label"] }),
-  });
-
-  const height =
-    Math.max(Math.min(pageSize, visibleOptions.length), 1) * itemHeight;
+const ComboBox = forwardRef(({ children, size = "md", ...props }, ref) => {
+  const { sizes } = useTheme();
+  const height = inputSizes[size] && inputSizes[size]["height"];
+  let pl = null;
+  let pr = null;
+  let right = null;
+  const validChildren = cleanChildren(children);
 
   return (
-    <div
-      style={{
-        display: "inline-block",
-        position: "relative",
-      }}
-    >
-      {multi ? (
-        <div>
-          {selectedOption.map(option => (
-            <div key={option.value}>
-              {option.value}{" "}
-              <span
-                onClick={() => onChange(value.filter(d => d !== option.value))}
-              >
-                x
-              </span>
-            </div>
-          ))}
-        </div>
-      ) : null}
-      <input {...getInputProps()} placeholder="Select one..." />
-      <OptionsWrapper ref={optionsRef}>
-        {isOpen ? (
-          <Options
-            ref={reactWindowInstanceRef}
-            height={height}
-            itemCount={visibleOptions.length || 1}
-            itemSize={itemHeight}
-            width={400}
-          >
-            {React.forwardRef(({ index, style, ...rest }, ref) => {
-              const option = visibleOptions[index];
-              if (!visibleOptions.length) {
-                return (
-                  <Option ref={ref} style={style}>
-                    No options were found...
-                  </Option>
-                );
-              }
-              return (
-                <Option
-                  {...getOptionProps({
-                    index,
-                    option,
-                    ref,
-                    style,
-                    highlighted: option === highlightedOption,
-                    selected: option === selectedOption,
-                  })}
-                >
-                  {option.label}
-                </Option>
-              );
-            })}
-          </Options>
-        ) : null}
-      </OptionsWrapper>
-    </div>
-  );
-};
+    <Box display="flex" position="relative" ref={ref} {...props}>
+      {validChildren.map(child => {
+        if (child.type === ComboBoxLeftElement) {
+          pl = height;
+        }
 
-export { MySelect };
+        if (child.type === ComboBoxRightElement) {
+          pr = height;
+        }
+
+        if (child.type === ComboBoxClearElement) {
+          right = sizes[height];
+          pr += height;
+
+          return cloneElement(child, { size, right });
+        }
+
+        if (child.type === ComboBoxInput) {
+          return cloneElement(child, {
+            size,
+            pl: child.props.pl || pl,
+            pr: child.props.pr || pr,
+          });
+        }
+        return cloneElement(child, { size });
+      })}
+    </Box>
+  );
+});
+
+const ComboBoxInput = forwardRef((props, ref) => {
+  return <Input ref={ref} cursor="default" {...props}></Input>;
+});
+
+const ComboBoxRightElement = forwardRef((props, ref) => {
+  return (
+    <InputRightElement
+      ref={ref}
+      children={<Icon name="chevron-down" fontSize="1.5rem" />}
+      pointerEvents="none"
+      {...props}
+    />
+  );
+});
+
+const ComboBoxLeftElement = forwardRef((props, ref) => {
+  return <InputLeftElement pointerEvents="none" ref={ref} {...props} />;
+});
+
+const ComboBoxLeftAddon = forwardRef((props, ref) => {
+  return <InputLeftAddon ref={ref} {...props} />;
+});
+
+const ComboBoxRightAddon = forwardRef((props, ref) => {
+  return <InputRightAddon ref={ref} {...props} />;
+});
+
+const ComboBoxClearElement = forwardRef((props, ref) => {
+  return (
+    <InputRightElement
+      ref={ref}
+      children={<Icon name="close" fontSize="12px" />}
+      cursor="default"
+      pointerEvents="none"
+      {...props}
+    />
+  );
+});
+
+export {
+  ComboBox,
+  ComboBoxInput,
+  ComboBoxLeftElement,
+  ComboBoxRightElement,
+  ComboBoxLeftAddon,
+  ComboBoxRightAddon,
+  ComboBoxClearElement,
+};
